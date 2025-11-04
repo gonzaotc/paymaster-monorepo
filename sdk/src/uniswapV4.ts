a4adcb3dffbbc362195b8a06b25e5d2a248c57d5
Gonzalo Othacehe <gonza.otc@gmail.com>
2025-11-04 19:06:20 -0300

    add mainnet forking

--- sdk/src/uniswapV4.ts
+++ sdk/src/uniswapV4.ts
@@ -1,4 +1,3 @@
-
 import { Address, encodeAbiParameters, Hash, keccak256 } from 'viem';
 
 /**
@@ -18,32 +17,26 @@ export type PoolKey = {
 };
 
 interface UniswapV4 {
-    /**
-     * Converts a pool key to a pool id.
-     * @param poolKey - The pool key to convert.
-     * @returns The pool id.
-     */
-    toId(poolKey: PoolKey): Hash;
+	/**
+	 * Converts a pool key to a pool id.
+	 * @param poolKey - The pool key to convert.
+	 * @returns The pool id.
+	 */
+	toId(poolKey: PoolKey): Hash;
 }
 
 export const uniswapV4: UniswapV4 = {
-    toId(poolKey: PoolKey): Hash {
-        const encoded = encodeAbiParameters(
-            [
-                { type: 'address' },
-                { type: 'address' },
-                { type: 'uint24' },
-                { type: 'int24' },
-                { type: 'address' }
-            ],
-            [
-                poolKey.currency0,
-                poolKey.currency1,
-                poolKey.fee,
-                poolKey.tickSpacing,
-                poolKey.hooks
-            ]
-        );
-        return keccak256(encoded);
-    }
-}
+	toId(poolKey: PoolKey): Hash {
+		const encoded = encodeAbiParameters(
+			[
+				{ type: 'address' },
+				{ type: 'address' },
+				{ type: 'uint24' },
+				{ type: 'int24' },
+				{ type: 'address' },
+			],
+			[poolKey.currency0, poolKey.currency1, poolKey.fee, poolKey.tickSpacing, poolKey.hooks]
+		);
+		return keccak256(encoded);
+	},
+};
